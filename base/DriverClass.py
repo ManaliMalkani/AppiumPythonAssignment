6c47b62d19055271fd10d373642346663b569d25
Manali Malkani <mamalkani@mafcarrefour.com>
2025-06-21 04:54:43 +0400

    Changes commit

--- base/DriverClass.py
+++ base/DriverClass.py
@@ -1,3 +1,5 @@
+import os
+
 from appium.options.android import UiAutomator2Options
 from appium import  webdriver
 
@@ -6,12 +8,18 @@ from appium import  webdriver
 class Driver:
 
     def getDriverMethod(self):
+        # Dynamically get the root path of the project
+        root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+        # Build full path to the APK file
+        app_path = os.path.join(root_dir, 'apps', 'Android_Demo_App.apk')
+
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
         desired_caps['automationName'] = 'UiAutomator2'
         desired_caps['platformVersion'] = '14'
         desired_caps['deviceName'] = 'Test'
-        desired_caps['app'] = ('/Users/mamalkani/Downloads/Android_Demo_App.apk')
+        desired_caps['app'] = app_path
         desired_caps['appPackage'] = 'com.code2lead.kwad'
         desired_caps['appActivity'] = 'com.code2lead.kwad.MainActivity'
         desired_caps['fullReset'] = True
